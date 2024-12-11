bf7d275bfe2d1d057aba08aa150b901db5b5ad78
Mohd4Nasir <mdnasir.6321@gmail.com>
2024-12-11 12:54:41 +0530

    commit to check

--- Screen/Asset/AssetRequest.tsx
+++ Screen/Asset/AssetRequest.tsx
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native';
 export default function AssetModule() {
   return (
     <View>
-      <Text>Asset Module</Text>
+      <Text>AssetModule</Text>
      </View>
   );
 }
